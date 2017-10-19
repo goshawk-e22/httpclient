f5f03b2424d51112dd32298e49475e8f26b10ab9
goshawk-e22 <32879862+goshawk-e22@users.noreply.github.com>
2017-10-19 17:28:34 +0800

    Getting started

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular Application test';
+  content = 'I wanna talk about how to build and develop for the Angular.';
+}
